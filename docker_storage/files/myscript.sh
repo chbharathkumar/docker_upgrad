7e47bf6108a9b2b4d58e5aef7df59f68bd15fd3c
chbharathkumar <bharath.ch203@gmail.com>
2020-05-04 17:14:28 +0530

    Update myscript.sh

--- docker_storage/files/myscript.sh
+++ docker_storage/files/myscript.sh
@@ -1,6 +1,17 @@
+sudo pvcreate /dev/sdb
+sudo vgcreate docker /dev/sdb
+sudo lvcreate --wipesignatures y -n thinpool docker -l 95%VG
+sudo lvcreate --wipesignatures y -n thinpoolmeta docker -l 1%VG
+sudo lvconvert -y \
+--zero n \
+-c 512K \
+--thinpool docker/thinpool \
+--poolmetadata docker/thinpoolmeta
+sudo lvchange --metadataprofile docker-thinpool docker/thinpool
+
 sudo rm -rf /var/lib/docker
 sudo mkdir /var/lib/docker
-sudo lvremove /dev/docker/thinpool 
+sudo lvremove /dev/docker/thinpool -y
 sudo lvcreate -n dockerdata -l 100%FREE docker -y
 sudo mkfs -t xfs -n ftype=1 /dev/docker/dockerdata
 sudo mount /dev/docker/dockerdata /var/lib/docker
